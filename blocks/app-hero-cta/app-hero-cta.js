09cebe048fb7af3e218043a826dd473ddcc9bf83
adisharm <adisharm@adobe.com>
2025-05-12 22:24:52 +0530

    Test blokc generation code

---
+++ blocks/app-hero-cta/app-hero-cta.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+    const buttons = block.querySelectorAll('.btn');
+    buttons.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        console.log(`Clicked: ${btn.textContent}`);
+      });
+    });
+  }
+  
