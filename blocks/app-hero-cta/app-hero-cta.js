b8fa5bb1ed07206c77d4fd18953ad84467af1030
adisharm <adisharm@adobe.com>
2025-05-13 09:10:42 +0530

    Update app-hero-cta.js

--- blocks/app-hero-cta/app-hero-cta.js
+++ blocks/app-hero-cta/app-hero-cta.js
@@ -1,1 +1,8 @@
-
+export default function decorate(block) {
+    const buttons = block.querySelectorAll('.btn');
+    buttons.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        console.log(`Clicked: ${btn.textContent}`);
+      });
+    });
+  }
