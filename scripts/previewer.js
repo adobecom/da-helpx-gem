cdb976f76e7c76ce4a4379987373a9b1388b9652
adisharm <adisharm@adobe.com>
2025-05-15 00:57:55 +0530

    add metadata block

--- scripts/previewer.js
+++ scripts/previewer.js
@@ -69,6 +69,8 @@ async function initiatePreviewer(source, contentUrl, editable, target, targetUrl
 }
 
 async function paintHtmlOnPage(html, source, contentUrl, target, targetUrl) {
+
+    populateMetadataBlock(html)
     const mainEle = document.createElement('main');
     const wrappingDiv = document.createElement('div');
     wrappingDiv.innerHTML = html;
@@ -91,6 +93,10 @@ async function paintHtmlOnPage(html, source, contentUrl, target, targetUrl) {
     // pushToDABtn.addEventListener('click', async () => {
     //     await persist(source, contentUrl, target, targetUrl);
     // });
+}
+
+function populateMetadataBlock(html) {
+    html += '<div class="metadata"> <div> <div><p>Title</p></div> <div><p>Customer Success Stories</p></div> </div> <div> <div><p>robots</p></div> <div><p>noodp</p></div> </div> <div> <div><p>description</p></div> <div> <p> Read our customer case studies and discover how Adobe Experience Cloud, Creative Cloud, and Document Cloud help the worlds leading brands. </p> </div> </div> <div> <div><p>keywords</p></div> <div><p>Experience Cloud</p></div> </div> <div> <div><p>serp-content-type</p></div> <div><p>case</p></div> </div> <div> <div><p>pageCreatedAt</p></div> <div><p>en</p></div> </div> <div> <div><p>translated</p></div> <div><p>false</p></div> </div> <div> <div><p>publishDate</p></div> <div><p>21-04-2022</p></div> </div> <div> <div><p>productJcrID</p></div> <div><p>products:SG_EXPERIENCECLOUD</p></div> </div> <div> <div><p>primaryProductName</p></div> <div><p>Experience Cloud</p></div> </div> <div> <div><p>data-event-swan-sessions-endpoint</p></div> <div></div> </div> <div> <div><p>data-jarvis-demandbase-enabled</p></div> <div><p>true</p></div> </div> <div> <div><p>entity_id</p></div> <div></div> </div> <div> <div><p>#teeside</p></div> <div> <p> https://business.adobe.com/fragments/customer-success-stories/modals/teesside-university </p> </div> </div> <div> <div><p>Tags</p></div> <div><p>Firefly</p></div> </div> </div>'
 }
 
 // TODO: manage error handling
