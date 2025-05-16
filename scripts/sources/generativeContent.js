70f4cec0aebc28bd277b23372298eea2db571185
mathuria <mathuria@adobe.com>
2025-05-16 19:25:39 +0530

    Generative ai enigma changes

--- scripts/sources/generativeContent.js
+++ scripts/sources/generativeContent.js
@@ -1,13 +1,4 @@
 export async function mapGenerativeContent(html, generativeContent) {
     console.log(generativeContent);
-    function delay(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    async function run() {
-      console.log("Start");
-      await delay(2000);
-      console.log("Executed after 2 seconds");
-    }
-    await run();
     return html;
 }
