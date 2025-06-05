d85f637021136807465cb426f977f0af41fc6142
mathuria <mathuria@adobe.com>
2025-06-05 12:21:10 +0530

    heading fix

--- scripts/sources/figma.js
+++ scripts/sources/figma.js
@@ -171,9 +171,11 @@ async function fetchContent(contentUrl) {
         } catch (error) {
             document.body.innerHTML = `<div class="enigma-error-page">
                                         <img src = "https://enigma--cc--aishwaryamathuria.aem.live/enigma/assets/errorgif.webp">
-                                        <h1> Oops!! Something broke.</h1>
-                                        <h1> It wasn’t me this time.</h1>
-                                        <h1> Probably...</h1>
+                                        <div>
+                                          <h1> Oops!! Something broke.</h1>
+                                          <h1> It wasn’t me this time.</h1>
+                                          <h1> Probably...</h1>
+                                        </div>
                                       </div>`
             console.error('Error fetching content:', error);
             return null;
