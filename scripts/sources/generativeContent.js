c4e55f5cb34b6748ef71d1d17c7a912339a3ee19
mathuria <mathuria@adobe.com>
2025-06-05 09:03:30 +0530

    Image upload break fix

--- scripts/sources/generativeContent.js
+++ scripts/sources/generativeContent.js
@@ -5,21 +5,25 @@ import { changeAsideContent } from "../blocks/aside.js";
 
 export async function mapGenerativeContent(html, blockMapping, generativeContent) {
   blockMapping.details.components.forEach((b, idx) => {
-    switch(b.id) {
-        case 'marquee': 
-            html = changeMarqueeContent(html, b.blockDomEl, generativeContent[idx]["Marquee"]);
-            break;
-        case 'text':
-            changeTextContent(html, b.blockDomEl, generativeContent[idx]["Text"]);
-            break;
-        case 'media':
-            changeMediaContent(html, b.blockDomEl, generativeContent[idx]["Media"]);
-            break;
-        case 'aside':
-            changeAsideContent(html, b.blockDomEl, generativeContent[idx]["Aside"]);
-            break;
-        default:
-            break;
+    try {
+      switch(b.id) {
+          case 'marquee': 
+              html = changeMarqueeContent(html, b.blockDomEl, generativeContent[idx]["Marquee"]);
+              break;
+          case 'text':
+              changeTextContent(html, b.blockDomEl, generativeContent[idx]["Text"]);
+              break;
+          case 'media':
+              changeMediaContent(html, b.blockDomEl, generativeContent[idx]["Media"]);
+              break;
+          case 'aside':
+              changeAsideContent(html, b.blockDomEl, generativeContent[idx]["Aside"]);
+              break;
+          default:
+              break;
+      }
+    } catch (err) {
+      console.log("⚠️ Failed to change content for a block");
     }
   });
 }
